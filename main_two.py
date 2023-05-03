c9c748b41501291aa3a2bdadc1c0ffb1ecf11843
Gilad Zelzer <giladz@microsoft.com>
2023-05-03 22:52:36 +0300

    wip

--- main_two.py
+++ main_two.py
@@ -1,5 +1,6 @@
 
 from logic_two import logic_two_print
+from testA.logic import logic_print
 
 def test_two_print(val : str):
     print("Hey_two " + val)
@@ -9,4 +10,5 @@ test_two_print(value)
 
 logic_two_print(value)
 
+logic_print(value)
 
